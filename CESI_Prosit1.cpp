fe85cdbe764b6cc83821f87d8958e7f4fb4d9e41
Jerem2360 <jlefoulon@hotmail.com>
2023-11-07 12:09:10 +0100

    documented the class

--- CESI_Prosit1.cpp
+++ CESI_Prosit1.cpp
@@ -7,9 +7,14 @@
 
 int main()
 {
+    // initial coordinates are {3, 0} and {0, 4}
     Point a(3, 0);
     Point b(0, 4);
     std::cout << "La distance qui sépare A et B est " << a.distance(b) << "\n";
-    b.move(1, 1);
+    // 5
+    a.move(-3, 0);
+    b.move(1, -3);
+    // new coordinates are {0, 0} and {1, 1}
     std::cout << "La distance qui sépare A et B est " << a.distance(b) << "\n";
+    // 1.414 (sqrt(2))
 }
--- Point.hpp
+++ Point.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
 
+/*
+Represents a point in 2d orthnormal coordinate system.
+*/
 class Point {
 	double x, y;
 
 public:
+	/*
+	Initialize a new Point object with the specified x and y coordinates.
+	*/
 	Point(double x, double y);
+	/*
+	Calculate the distance between this Point and the specified point.
+	*/
 	double distance(Point other);
+	/*
+	Move this Point by the specified number of units in the x and y coordinates.
+	*/
 	void move(double x, double y);
 };
 
