1ae3ca06ad526a34cf7171559f7fff6e3759fe47
Jerem2360 <jlefoulon@hotmail.com>
2023-11-12 22:05:02 +0100

    clarified the source code.

--- Point.cpp
+++ Point.cpp
@@ -5,7 +5,7 @@
 Point::Point(double x, double y) : x(x), y(y) {}
 
 double Point::distance(Point other) {
-    return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+    return std::sqrt((this->x - other.x) * (this->x - other.x) + (this->y - other.y) * (this->y - other.y));
 }
 
 void Point::move(double x, double y) {
